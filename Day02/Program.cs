7f04e4a52d8d215bf9a4612bd0f8b40b1cccdfe4
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-02 08:42:56 -0600

    Day 02

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -4,6 +4,7 @@
     {
         public static string _input = "";
         public static List<string> entries = new();
+
         static void Main()
         {
             _input = File.ReadAllText("Input.txt");
@@ -14,10 +15,68 @@
 
         static void Part1()
         {
+            int score = 0;
+            foreach(var l in entries)
+            {
+                var moves = l.Split(" ");
+                switch (moves[1]) {
+                    case "X":
+                        score+=1;
+                        break;
+                    case "Y":
+                        score+=2;
+                        break;
+                    case "Z":
+                        score+=3;
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke! moves[1] = " + moves[1]);
+                        break;
+                }
+                if (moves[0] == "A")
+                    if (moves[1] == "X") score += 3;
+                    else if (moves[1] == "Y") score += 6;
+                    else if (moves[1] == "Z") continue;
+                    else Console.WriteLine("Something Broke!");
+                else if (moves[0] == "B")
+                    if (moves[1] == "Y") score += 3;
+                    else if (moves[1] == "Z") score += 6;
+                    else if (moves[1] == "X") continue;
+                    else Console.WriteLine("Something Broke!");
+                else if (moves[0] == "C")
+                    if (moves[1] == "Z") score += 3;
+                    else if (moves[1] == "X") score += 6;
+                    else if (moves[1] == "Y") continue;
+                    else Console.WriteLine("Something Broke!");
+                else Console.WriteLine("Something Broke!");
+            }
+            Console.WriteLine("Final score is " + score);
         }
 
         static void Part2()
         {
+            int score = 0;
+            foreach (var l in entries)
+            {
+                var moves = l.Split(" ");
+                if (moves[0] == "A")
+                    if (moves[1] == "X") score += 3;
+                    else if (moves[1] == "Y") score += 4;
+                    else if (moves[1] == "Z") score += 8;
+                    else Console.WriteLine("Something Broke!");
+                else if (moves[0] == "B")
+                    if (moves[1] == "X") score += 1;
+                    else if (moves[1] == "Y") score += 5;
+                    else if (moves[1] == "Z") score += 9;
+                    else Console.WriteLine("Something Broke!");
+                else if (moves[0] == "C")
+                    if (moves[1] == "X") score += 2;
+                    else if (moves[1] == "Y") score += 6;
+                    else if (moves[1] == "Z") score += 7;
+                    else Console.WriteLine("Something Broke!");
+                else Console.WriteLine("Something Broke!");
+            }
+            Console.WriteLine("Final score is " + score);
         }
     }
 }
