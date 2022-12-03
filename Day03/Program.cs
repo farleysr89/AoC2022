4538598310e90114ddb229de57177283e52dbb54
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-03 10:06:06 -0600

    Day03 complete

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -14,10 +14,26 @@
 
         static void Part1()
         {
+            var priorities = (from s in entries let l = s.Length / 2 select s[0..l].Intersect(s[l..]).First() into c select GetPriority(c)).Sum();
+            Console.WriteLine("Sum of priorities is " + priorities);
         }
 
         static void Part2()
         {
+            var priorities = 0;
+            var l = entries.Count;
+            for (var i = 0; i < l; i += 3)
+            {
+                var c = entries[i].Intersect(entries[i + 1]).Intersect(entries[i + 2]).First();
+                priorities += GetPriority(c);
+            }
+            Console.WriteLine("Sum of priorities is " + priorities);
+        }
+
+        static int GetPriority(char c)
+        {
+            var i = (int)c - 64;
+            return i + (i <= 26 ? 26 : -32);
         }
     }
 }
