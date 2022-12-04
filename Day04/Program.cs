5d9f12484cf0c92163fbe1da91dddcea9ec7ed6b
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-04 05:38:01 -0600

    Day04 complete

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -14,12 +14,41 @@
 
         static void Part1()
         {
-
+            var overlaps = 0;
+            foreach(var l in entries)
+            {
+                if(IsFullOverlap(l)) overlaps++;
+            }
+            Console.WriteLine("Count of overlapping shifts is " + overlaps);
         }
 
         static void Part2()
         {
-
+            var overlaps = 0;
+            foreach(var l in entries)
+            {
+                if(IsPartialOverlap(l)) overlaps++;
+            }
+            Console.WriteLine("Count of overlapping shifts is " + overlaps);
         }
+        static bool IsFullOverlap(string l)
+        {
+            var assignments = l.Split(",");
+            var sections1 = assignments[0].Split("-").Select(s => int.Parse(s)).ToList();
+            var sections2 = assignments[1].Split("-").Select(s => int.Parse(s)).ToList();
+            return (sections1[0] >= sections2[0] && sections1[1] <= sections2[1])
+                || (sections2[0] >= sections1[0] && sections2[1] <= sections1[1]);
+        }
+        static bool IsPartialOverlap(string l)
+        {
+            var assignments = l.Split(",");
+            var sections1 = assignments[0].Split("-").Select(s => int.Parse(s)).ToList();
+            var sections2 = assignments[1].Split("-").Select(s => int.Parse(s)).ToList();
+            return (sections1[1] >= sections2[0] && sections1[1] <= sections2[1])
+                || (sections1[0] >= sections2[0] && sections1[0] <= sections2[1])
+                || (sections2[1] >= sections1[0] && sections2[1] <= sections1[1])
+                || (sections2[0] >= sections1[0] && sections2[0] <= sections1[1]);
+        }
+
     }
 }
