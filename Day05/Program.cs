7b00236e81f4ea6d66bb13de5ad71398b13ee679
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-05 10:36:40 -0600

    Day 5 Complete

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2022
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AoC2022
 {
     internal class Program
     {
@@ -14,12 +16,92 @@
 
         static void Part1()
         {
-
+            var length = entries[0].Length + 1;
+            var stacks = new List<Stack<char>>();
+            var j = 0;
+            foreach (var l in entries)
+            {
+                if (!l.Contains('[')) break;
+                j = 0;
+                for (var i = 0; i < length; i += 4)
+                {
+                    if (stacks.Count <= j) stacks.Add(new Stack<char>());
+                    if (l[i + 1] != ' ') stacks[j].Push(l[i + 1]);
+                    j++;
+                }
+            }
+            for (var i = 0; i < stacks.Count; i++)
+            {
+                stacks[i] = reverseStack(stacks[i]);
+            }
+            foreach (var l in entries.Skip(j + 1))
+            {
+                var parts = l.Split(" ");
+                var count = int.Parse(parts[1]);
+                var from = int.Parse(parts[3]) - 1;
+                var to = int.Parse(parts[5]) - 1;
+                for (var i = 0; i < count; i++)
+                {
+                    stacks[to].Push(stacks[from].Pop());
+                }
+            }
+            var topCrates = "";
+            foreach (var s in stacks)
+            {
+                topCrates += s.Pop();
+            }
+            Console.WriteLine("Top crates are " + topCrates);
         }
 
         static void Part2()
         {
+            var length = entries[0].Length + 1;
+            var stacks = new List<Stack<char>>();
+            var j = 0;
+            foreach (var l in entries)
+            {
+                if (!l.Contains('[')) break;
+                j = 0;
+                for (var i = 0; i < length; i += 4)
+                {
+                    if (stacks.Count <= j) stacks.Add(new Stack<char>());
+                    if (l[i + 1] != ' ') stacks[j].Push(l[i + 1]);
+                    j++;
+                }
+            }
+            for (var i = 0; i < stacks.Count; i++)
+            {
+                stacks[i] = reverseStack(stacks[i]);
+            }
+            foreach (var l in entries.Skip(j + 1))
+            {
+                var parts = l.Split(" ");
+                var count = int.Parse(parts[1]);
+                var from = int.Parse(parts[3]) - 1;
+                var to = int.Parse(parts[5]) - 1;
+                var tempStack = new Stack<char>();
+                for (var i = 0; i < count; i++)
+                {
+                    tempStack.Push(stacks[from].Pop());
+                }
+                while(tempStack.Count > 0)
+                {
+                    stacks[to].Push(tempStack.Pop());
+                }
+            }
+            var topCrates = "";
+            foreach (var s in stacks)
+            {
+                topCrates += s.Pop();
+            }
+            Console.WriteLine("Top crates are " + topCrates);
+        }
 
+        static Stack<char> reverseStack(Stack<char> stack)
+        {
+            var reverse = new Stack<char>();
+            while (stack.Count > 0) reverse.Push(stack.Pop());
+            return reverse;
         }
     }
 }
