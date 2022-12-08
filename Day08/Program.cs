70a8b5b5381b9f3e0c34f3ab182f6d948d6b2805
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-08 14:02:39 -0600

    Day 8 Part 1

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -4,6 +4,7 @@
     {
         public static string _input = "";
         public static List<string> entries = new();
+        public static List<Tree> trees = new();
         static void Main()
         {
             _input = File.ReadAllText("Input.txt");
@@ -14,10 +15,49 @@
 
         static void Part1()
         {
+            var visibleTrees = 0;
+            visibleTrees += entries.Count * 2;
+            visibleTrees += (entries[0].Length - 2) * 2;
+            for(var i = 0; i < entries.Count; i++)
+            {
+                for(var j = 0; j < entries[i].Length; j++)
+                {
+                    var tree = entries[i][j];
+                    trees.Add(new Tree
+                    {
+                        X = j,
+                        Y = i,
+                        Height = int.Parse(tree.ToString())
+                    });
+                }
+            }
+            foreach(var tree in trees)
+            {
+                if(tree.X == 0 || tree.X == entries.Count - 1 || tree.Y == 0 || tree.Y == entries[0].Length - 1)
+                {
+                    continue;
+                }
+                if(!trees.Any(t => t.X == tree.X && t.Y < tree.Y && t.Height >= tree.Height)
+                || !trees.Any(t => t.X < tree.X && t.Y == tree.Y && t.Height >= tree.Height)
+                || !trees.Any(t => t.X == tree.X && t.Y > tree.Y && t.Height >= tree.Height)
+                || !trees.Any(t => t.X > tree.X && t.Y == tree.Y && t.Height >= tree.Height))
+                {
+                    visibleTrees++;
+                }
+            }
+            Console.WriteLine("There are " + visibleTrees + " visible trees.");
         }
 
         static void Part2()
         {
         }
+    }
+
+    internal class Tree
+    {
+        public int X;
+        public int Y;
+        public int Height;
+
     }
 }
