913dfe7e749c874cbaee9f4a68230ee1fa55a671
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-15 15:10:21 -0600

    Day 10 Part 2 WIP

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2022
+﻿using System.Globalization;
+
+namespace AoC2022
 {
     internal class Program
     {
@@ -45,6 +47,42 @@
 
         static void Part2()
         {
+            var X = 1;
+            var CycleCounter = 0;
+            var SignalStrengths = new int[241];
+            SignalStrengths[0] = 1;
+            foreach (var entry in entries)
+            {
+                if (CycleCounter >= 240) break;
+                var parts = entry.Split(" ");
+                switch (parts[0])
+                {
+                    case "addx":
+                        CycleCounter++;
+                        SignalStrengths[CycleCounter] = X;
+                        CycleCounter++;
+                        SignalStrengths[CycleCounter] = X;
+                        X += int.Parse(parts[1]);
+                        break;
+                    case "noop":
+                        CycleCounter++;
+                        SignalStrengths[CycleCounter] = X;
+                        break;
+                    default:
+                        Console.WriteLine("Something broke!");
+                        return;
+                }
+            }
+            for (var j = 0; j < 6;j++)
+            {
+                var output = "";
+                for(var i = 1 + j * 40; i <= (j + 1) * 40; i++)
+                {
+                    output += SignalStrengths[i] == i - 1 || SignalStrengths[i] == i || SignalStrengths[i] == i - 2 ? '#' : '.';
+                }
+                Console.WriteLine(output);
+            }
+            //Console.WriteLine("Solution = {0}", (20 * SignalStrengths[20] + 60 * SignalStrengths[60] + 100 * SignalStrengths[100] + 140 * SignalStrengths[140] + 180 * SignalStrengths[180] + 220 * SignalStrengths[220]));
         }
     }
 }
