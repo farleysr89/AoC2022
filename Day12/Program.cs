da3a29e6bcfb1042ce8c605d57d34c8f2ccf82df
Farley <33631912+farleysr89@users.noreply.github.com>
2022-12-16 20:53:37 -0600

    Day 12 Part 1

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -4,6 +4,7 @@
     {
         public static string _input = "";
         public static List<string> entries = new();
+        public static List<Location> locations = new List<Location>();
         static void Main()
         {
             _input = File.ReadAllText("Input.txt");
@@ -14,10 +15,76 @@
 
         static void Part1()
         {
+            var pos = 'a';
+            var dest = 'z';
+            var y = 0;            
+            foreach(var entry in entries)
+            {
+                var x = 0;
+                foreach(var c in entry)
+                {
+                    locations.Add(new Location
+                    {
+                        X = x,
+                        Y = y,
+                        Height = c == 'S' ? 'a' : c == 'E' ? 'z' : c,
+                        Start = c == 'S',
+                        End = c == 'E'
+                    });
+                    x++;
+                }
+                y++;
+            }
+            var start = locations.First(l => l.Start);
+            start.Move(0);
+            var end = locations.First(l => l.End);
+            Console.WriteLine("Shortest Distance = " + end.ShortestDistance);
         }
 
         static void Part2()
         {
+        }
+
+        internal class Location
+        {
+            public int X;
+            public int Y;
+            public char Height;
+            public bool Start;
+            public bool End;
+            public int ShortestDistance = -1;
+
+            public void Move(int moves)
+            {
+                if(ShortestDistance == -1 || moves < ShortestDistance) ShortestDistance = moves;
+                else return;
+                if(End) return;
+                moves++;
+                var loc = locations.FirstOrDefault(l => l.X == X - 1 && l.Y == Y);
+                if(loc != null)
+                {
+                    if(loc.Height <= Height+ 1)
+                        loc.Move(moves);
+                }
+                loc = locations.FirstOrDefault(l => l.X == X + 1 && l.Y == Y);
+                if(loc != null)
+                {
+                    if(loc.Height <= Height+ 1)
+                        loc.Move(moves);
+                }
+                loc = locations.FirstOrDefault(l => l.X == X && l.Y == Y - 1);
+                if(loc != null)
+                {
+                    if(loc.Height <= Height+ 1)
+                        loc.Move(moves);
+                }
+                loc = locations.FirstOrDefault(l => l.X == X && l.Y == Y + 1);
+                if(loc != null)
+                {
+                    if(loc.Height <= Height+ 1)
+                        loc.Move(moves);
+                }
+            }
         }
     }
 }
